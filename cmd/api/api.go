83f1749ebcb150370c76d4bfb288d5bd013ac95f
JerryLegend254 <jerryokuto713@gmail.com>
2025-03-04 13:32:29 +0300

    chore: ws impl

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
 
@@ -15,11 +17,39 @@ type config struct {
 	addr string
 }
 
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+func hello(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
+}
+
 func (app *application) mount() http.Handler {
 	e := echo.New()
 	r := e.Group("/v1")
 
 	r.GET("/ping", app.pingHandler)
+	r.GET("/ws", hello)
 
 	return e
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/JerryLegend254/CollabGo
 
 go 1.23.4
 
-require github.com/labstack/echo/v4 v4.13.3
+require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/labstack/echo/v4 v4.13.3
+)
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
